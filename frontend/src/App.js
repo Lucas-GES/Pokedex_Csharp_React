4d99f705635a8b49bd1e27e2e862beada940e9fd
Lucas Santo <lucas.santo@elitesoft.com.br>
2024-08-13 17:58:14 -0300

    Iniciado frontend do projeto, adicionada page inicial com a pokedex

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Pokemons from './views/Pokemons';
+function App() {
+  return (
+    <div className="App">
+        <Pokemons></Pokemons>
+    </div>
+  );
+}
+
+export default App;
